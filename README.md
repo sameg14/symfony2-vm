498a0ce0823ab698bcc9c5ccb5483fe4e074a3fd
Samir Patel <samir.patel@wholefoods.com>
2015-10-01 14:33:50 -0500

    Updated instructions

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # Instructions to install VM
 
-## Easy Way
-The easy way
 ```bash
-sh 
+./install.sh
 ```
 
-## Hard way
+The install script will make a folder on your desktop called symfony.vm
+
+Once the install completes, you can visit (http://10.10.10.11)[http://10.10.10.11] in your browser
+
+The VM has both production and development environments configured in apache. 
+
+Add the following entries to ```/etc/hosts``` on your mac
+```bash
+10.10.10.24     symfony2.dev
+10.10.10.24     symfony2.prod
+```
+
--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Halt any existing running vms
+cd ~/Desktop/symfony.vm
+vagrant halt
+vagrant destroy
+
 # Ensure vm folder exists and is empty
 cd ~/Desktop/
 rm -rf symfony.vm
@@ -7,4 +12,23 @@ mkdir symfony.vm
 cd symfony.vm
 
 # Download vagrant file
+curl -O https://raw.githubusercontent.com/sameg14/symfony2-vm/master/VagrantFile
 
+# Download and install symfony
+curl -LsS http://symfony.com/installer -o symfony.phar
+php symfony.phar new symfony2
+
+# Download composer and update packages
+cd symfony2
+curl -sS https://getcomposer.org/installer | php
+php composer.phar update -vvv
+
+# Create virtual machine
+cd ~/Desktop/symfony.vm
+vagrant up
+
+# Open the site in a browser
+open http://10.10.10.24
+
+# SSH into new VM
+vagrant ssh
